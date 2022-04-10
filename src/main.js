99e0bf3e76145ef9c596479489c54fef3cbe2ae9
AhmedSalheia <ahmedsalheia.as@gmail.com>
2022-04-10 11:50:27 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueHead from "vue-head";
 
 Vue.config.productionTip = false;
 
@@ -10,8 +9,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "@/assets/gapi";
 import vuetify from "./plugins/vuetify";
-
-Vue.use(VueHead);
 
 new Vue({
     router,
